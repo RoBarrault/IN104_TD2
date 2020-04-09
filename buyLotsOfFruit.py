6a4ef17148a8bde721bf67ea18099a9bd015b271
Romain BARRAULT <romain.barrault@ensta-paristech.fr>
2020-04-09 23:13:06 +0200

    Correction (je l'espère) des erreurs d'indentation

--- buyLotsOfFruit.py
+++ buyLotsOfFruit.py
@@ -29,23 +29,22 @@ fruitPrices = {'apples': 2.00, 'oranges': 1.50, 'pears': 1.75,
 
 
 def buyLotsOfFruit(orderList):
-    """
-        orderList: List of (fruit, numPounds) tuples
-
-    Returns cost of order
-    """
-    totalCost = 0.0
-    for k in range(len(orderList)):
-	(f,n) = orderList[k]
-	if f not in fruitPrices:
-		print("Sorry, we don't have that fruit.\n")
-		return None
-	totalCost += n*fruitPrices[f]
-    return totalCost
+	"""
+		orderList: List of (fruit, numPounds) tuples
+		Returns cost of order
+	"""
+	totalCost = 0.0
+	for k in range(len(orderList)):
+		(f,n) = orderList[k]
+		if f not in fruitPrices:
+			print("Sorry, we don't have that fruit.\n")
+			return None
+		totalCost += n*fruitPrices[f]
+	return totalCost
 
 
 # Main Method
 if __name__ == '__main__':
-    "This code runs when you invoke the script from the command line"
-    orderList = [('apples', 2.0), ('pears', 3.0), ('limes', 4.0)]
-    print('Cost of', orderList, 'is', buyLotsOfFruit(orderList))
+	"This code runs when you invoke the script from the command line"
+	orderList = [('apples', 2.0), ('pears', 3.0), ('limes', 4.0)]
+	print('Cost of', orderList, 'is', buyLotsOfFruit(orderList))
